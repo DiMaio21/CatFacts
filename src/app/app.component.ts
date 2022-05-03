fde7d638c2c3df67f07dc551f09cc1cb7f17f15b
DiMaio21 <dimaio.gabriele@iisgalvanimi.edu.it>
2022-05-03 19:16:06 +0000

    nograf

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+frase: string=""
+
+ constructor(private frasine: HttpClient)
+ {
+  this.frasine.get("https://catfact.ninja/fact").subscribe(
+  res=>{
+  this.frase= res["fact"]
+
+  }
+
+  )
+
+ }
+  title = 'CatFacts';
+
+  
+
+
+}
